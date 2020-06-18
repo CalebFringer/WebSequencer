f29a323ae2924db5d0c31a58e1aa449dfdf0d528
BuildTools <unconfigured@null.spigotmc.org>
2020-06-18 16:18:16 +0000

    Partially implemented styles.css

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sequencer from './components/Sequencer'
+import './styles.css';
 
-//ReactDOM.render(<App />, document.body);
-ReactDOM.render(
-	<Sequencer />, 
-	document.getElementById('root')
-);
+function App() {
+	return (
+		<div id='app'>
+			<h1>WebSequencer - by Caleb Fringer</h1>
+			<br/> <br/>
+			<Sequencer />
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
