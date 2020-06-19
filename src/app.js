5fb4e56295be001fb7e567cf41b84f5adda86b0d
BuildTools <unconfigured@null.spigotmc.org>
2020-06-19 17:40:55 +0000

    Moved the UI controls out of the build process. Sliders still don't work properly

--- src/app.js
+++ src/app.js
@@ -3,14 +3,4 @@ import ReactDOM from 'react-dom';
 import Sequencer from './components/Sequencer'
 import './styles.css';
 
-function App() {
-	return (
-		<div id='app'>
-			<h1>WebSequencer - by Caleb Fringer</h1>
-			<br/> <br/>
-			<Sequencer />
-		</div>
-	)
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Sequencer />, document.getElementById('root'));
