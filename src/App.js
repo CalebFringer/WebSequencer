fc00bc8b718296af422fb187667d7e6f3a8cfd13
CalebF98 <cfringer98@gmail.com>
2023-10-05 20:49:25 -0700

    Accidentally deleted index.html

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Keyboard from './components/Keyboard.js';
+import AudioControls from './components/AudioControls.js'
+
+class App extends React.Component {
+	render() {
+		return(
+			<div id="App">
+				<Keyboard />
+				<AudioControls />
+			</div>
+		);
+	}
+}
+
+export default App;
--- src/index..js
+++ src/index..js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Components
 import App from './App.js'
 
 ReactDOM.render(<App />, document.getElementById('root'));
